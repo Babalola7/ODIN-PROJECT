462005da837d3d3d98ea278cf7ac310af6425573
Babalola Idris <babalolaidris7@gmail.com>
2022-09-16 10:57:43 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+const formSubmit = document.getElementById('form')
+const passwordInput = document.getElementById('password')
+const passwordCheck = document.getElementById('password-check')
+const passwordIncorrect = document.getElementById('not-match')
+
+
+
+
+formSubmit.addEventListener('submit',(c) =>{
+    c.preventDefault();
+    console.log(*pressed*);
+    ConfirmPassword()
+})
+
+
+// password confirm check
+const ConfirmPassword = () =>(
+    if (passwordInput.value !== passwordCheck.value) {
+        passwordIncorrect.style.display = "block"
+        passwordInput.style.outline = "none"
+        passwordCheck.style.border = "1px solid red"
+        passwordInput.style.border = "1px solid red"
+    } else {
+        passwordIncorrect.style.display = "none"
+    }
+)
